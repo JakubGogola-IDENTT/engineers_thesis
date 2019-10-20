066ee6f397b17b315733ab2c37b3c649ab66ff18
JakubGogola <jakubgogola97@gmail.com>
2019-10-20 07:20:32 +0200

    getRandomColor method added

--- src/genetics/module/module.go
+++ src/genetics/module/module.go
@@ -1,34 +1,35 @@
 package module
 
 import (
-	"fmt"
-	"image/draw"
+	"image"
+	"image/color"
 	"math/rand"
 )
 
-func mutate(spec draw.Image) {
+func mutate(spec image.RGBA) {
 	// maximal values of x and y in given Image with border
 	borderX := spec.Bounds().Max.X - 1
 	borderY := spec.Bounds().Max.Y - 1
 
-	// x of left upper corner of mutated rectangle
+	// x and y of left upper corner of mutated rectangle
 	minX := rand.Intn(borderX)
-
-	// y of left upper corner of mutated rectangle
 	minY := rand.Intn(borderY)
 
-	// width of mutated rectangle
+	// width and height of mutated rectangle
 	width := rand.Intn(borderX - minX - 1)
-
-	// height of mutated rectangle
 	height := rand.Intn(borderY - minY + 1)
-
-	// color of mutated rectangle
-	fmt.Println(width, height)
 
 	for x := minX; x <= minX+width; x++ {
 		for y := minY; y <= minY+height; y++ {
-			spec.Set()
+			spec.Set(x, y, getRandomColor())
 		}
 	}
+}
+
+func getRandomColor() color.Color {
+	r := uint8(rand.Intn(256))
+	g := uint8(rand.Intn(256))
+	b := uint8(rand.Intn(256))
+
+	return color.RGBA{R: r, G: g, B: b, A: 255}
 }
